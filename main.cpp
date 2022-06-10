b880f50385329e3230de1406bfdde00340503d6f
Limerancy <limerancy@gmail.com>
2022-06-10 22:18:04 +0800

    Add main.cpp to gitignore

--- main.cpp
+++ main.cpp
@@ -16,58 +16,57 @@ AccountManagement accounts; //声明在外部，防止数组太大，爆栈空�
 TrainManagement trains;
 
 int main() {
-    string input;
+  string input;
 
-    freopen("test_data/normal/basic_2/1.in", "r", stdin);
-    freopen("output.txt", "w", stdout);
+  freopen("testdata/normal/basic_2/1.in", "r", stdin);
+  freopen("testdata/normal/basic_2/1.ans", "w", stdout);
 
+  while (getline(cin, input)) {
+    Command cmd(input);
+    string time = cmd.next_token();
+    int l = time.length();
+    cmd.timestamp = string_to_int(time.substr(1, l - 2));
+    //        commands.push_back(cmd);
 
-    while (getline(cin, input)) {
-        Command cmd(input);
-        string time = cmd.next_token();
-        int l = time.length();
-        cmd.timestamp = string_to_int(time.substr(1, l - 2));
-        //        commands.push_back(cmd);
+    cout << "[" << cmd.timestamp << "] "; //输出时间戳，方便调试
+    string s = cmd.next_token();
+    if (s == "add_user")
+      cout << accounts.add_user(cmd) << endl;
+    else if (s == "login")
+      cout << accounts.login(cmd) << endl;
+    else if (s == "logout")
+      cout << accounts.logout(cmd) << endl;
+    else if (s == "query_profile")
+      cout << accounts.query_profile(cmd) << endl;
+    else if (s == "modify_profile")
+      cout << accounts.modify_profile(cmd) << endl;
 
-        cout << "[" << cmd.timestamp << "] "; //输出时间戳，方便调试
-        string s = cmd.next_token();
-        if (s == "add_user")
-            cout << accounts.add_user(cmd) << endl;
-        else if (s == "login")
-            cout << accounts.login(cmd) << endl;
-        else if (s == "logout")
-            cout << accounts.logout(cmd) << endl;
-        else if (s == "query_profile")
-            cout << accounts.query_profile(cmd) << endl;
-        else if (s == "modify_profile")
-            cout << accounts.modify_profile(cmd) << endl;
+    else if (s == "add_train")
+      cout << trains.add_train(cmd) << endl;
+    else if (s == "release_train")
+      cout << trains.release_train(cmd) << endl;
+    else if (s == "query_train")
+      cout << trains.query_train(cmd) << endl;
+    else if (s == "delete_train")
+      cout << trains.delete_train(cmd) << endl;
+    else if (s == "query_ticket")
+      cout << trains.query_ticket(cmd) << endl;
+    else if (s == "query_transfer")
+      cout << trains.query_transfer(cmd) << endl; // trains.query_transfer(cmd)
 
-        else if (s == "add_train")
-            cout << trains.add_train(cmd) << endl;
-        else if (s == "release_train")
-            cout << trains.release_train(cmd) << endl;
-        else if (s == "query_train")
-            cout << trains.query_train(cmd) << endl;
-        else if (s == "delete_train")
-            cout << trains.delete_train(cmd) << endl;
-        else if (s == "query_ticket")
-            cout << trains.query_ticket(cmd) << endl;
-        else if (s == "query_transfer")
-            cout << trains.query_transfer(cmd) << endl; // trains.query_transfer(cmd)
+    else if (s == "buy_ticket")
+      cout << trains.buy_ticket(cmd, accounts) << endl;
+    else if (s == "query_order")
+      cout << trains.query_order(cmd, accounts) << endl;
+    else if (s == "refund_ticket")
+      cout << trains.refund_ticket(cmd, accounts) << endl;
+    else if (s == "rollback")
+      cout << trains.rollback(cmd, accounts) << endl;
+    else if (s == "clean")
+      cout << trains.clean(accounts) << endl;
+    else if (s == "exit")
+      trains.exit(accounts);
+  }
 
-        else if (s == "buy_ticket")
-            cout << trains.buy_ticket(cmd, accounts) << endl;
-        else if (s == "query_order")
-            cout << trains.query_order(cmd, accounts) << endl;
-        else if (s == "refund_ticket")
-            cout << trains.refund_ticket(cmd, accounts) << endl;
-        else if (s == "rollback")
-            cout << trains.rollback(cmd, accounts) << endl;
-        else if (s == "clean")
-            cout << trains.clean(accounts) << endl;
-        else if (s == "exit")
-            trains.exit(accounts);
-    }
-
-    return 0;
+  return 0;
 }
