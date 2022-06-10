3878221fd42b20fdf0df45c66072b787e647475a
John-Steve-C <leonardchen701@outlook.com>
2022-06-10 22:29:35 +0800

    v4.5.4

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ TrainManagement trains;
 int main() {
     string input;
 
-    freopen("test_data/normal/basic_3/1.in", "r", stdin);
+    freopen("test_data/normal/basic_3/2.in", "r", stdin);
     freopen("output.txt", "w", stdout);
 
 
