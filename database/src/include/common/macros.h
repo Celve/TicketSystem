7de460276ed086ce64a5c64cc3198a953e24c2e4
Limerancy <limerancy@gmail.com>
2022-06-10 21:09:55 +0800

    Add template definition for b+ tree

--- database/src/include/common/macros.h
+++ database/src/include/common/macros.h
@@ -4,6 +4,7 @@
 #include "TrainSystem.h"
 #include "type/dual_string.h"
 #include "type/string_any.h"
+#include "type/string_int_int.h"
 
 namespace thomas {
 
@@ -12,18 +13,22 @@ namespace thomas {
   class_name operator=(const class_name &) = delete;  // NOLINT
 
 #define DECLARE(class_name)                                                                           \
+  template class class_name<String<32>, size_t, StringComparator<32>>;                   /* NOLINT */ \
   template class class_name<String<48>, size_t, StringComparator<48>>;                   /* NOLINT */ \
   template class class_name<StringAny<68, int>, int, StringAnyComparator<68, int>>;      /* NOLINT */ \
   template class class_name<String<32>, User, StringComparator<32>>;                     /* NOLINT */ \
+  template class class_name<String<32>, Train, StringComparator<32>>;                    /* NOLINT */ \
   template class class_name<DualString<32, 32>, Station, DualStringComparator<32, 32>>;  /* NOLINT */ \
   template class class_name<StringAny<32, int>, DayTrain, StringAnyComparator<32, int>>; /* NOLINT */ \
-  template class class_name<StringAny<32, int>, Order, StringAnyComparator<32, int>>;    /* NOLINT */
+  template class class_name<StringAny<32, int>, Order, StringAnyComparator<32, int>>;    /* NOLINT */ \
+  template class class_name<StringIntInt<32>, PendingOrder, StringIntIntComparator<32>>; /* NOLINT */
 
 #define DECLARE_BPLUSTREE_INTERNALPAGE(class_name)                                                     \
   template class class_name<String<32>, page_id_t, StringComparator<32>>;                 /* NOLINT */ \
   template class class_name<String<48>, page_id_t, StringComparator<48>>;                 /* NOLINT */ \
   template class class_name<DualString<32, 32>, page_id_t, DualStringComparator<32, 32>>; /* NOLINT */ \
   template class class_name<StringAny<32, int>, page_id_t, StringAnyComparator<32, int>>; /* NOLINT */ \
-  template class class_name<StringAny<68, int>, page_id_t, StringAnyComparator<68, int>>; /* NOLINT */
+  template class class_name<StringAny<68, int>, page_id_t, StringAnyComparator<68, int>>; /* NOLINT */ \
+  template class class_name<StringIntInt<32>, page_id_t, StringIntIntComparator<32>>;     /* NOLINT */
 
 }  // namespace thomas
